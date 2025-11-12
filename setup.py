a51b0ec8959012b7caac18a9a6d0ab78abf2d3b1
rabindra-basnet <120715649+rabindra-basnet@users.noreply.github.com>
2025-11-12 12:07:26 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
-
 # get version from __version__ variable in jwt_frappe/__init__.py
 from jwt_frappe import __version__ as version
 
@@ -15,6 +12,5 @@ setup(
 	author_email='Ahmedragheb75@gmail.com',
 	packages=find_packages(),
 	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
+	include_package_data=True
 )
